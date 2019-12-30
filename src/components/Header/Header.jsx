827499052822213a5d8fcd2e712de0ba80a61eb8
Kirill Eremin <kirilleremin@yandex-team.ru>
2019-12-31 01:59:21 +0300

    add Background component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,16 @@
 import React, { Component } from 'react';
 import cn from 'classnames/bind';
 
-import headerJpg from './assets/header.jpg';
-import headerGif from './assets/header.gif';
-
 import * as styles from './Header.scss';
+import Background from "./parts/Background/Background";
+
 const cx = cn.bind(styles);
 
 class Header extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            loading: true,
-        };
-
-        const gif = new Image();
-        gif.onload = () => {
-            this.setState({
-                loading: false,
-            })
-        };
-        gif.src = headerGif;
-    }
-
     render() {
-        const { loading } = this.state;
-
-        const temporaryBackgroundStyles = {
-            backgroundImage: `url(${headerJpg})`,
-        };
-
-        const styles = {
-            backgroundImage: `url(${headerGif})`,
-        };
-
         return (
             <div className={cx('root')}>
-                {loading && (
-                    <div
-                        className={cx('background')}
-                        style={temporaryBackgroundStyles}
-                    />
-                )}
-                <div className={cx('background')} style={styles} />
+                <Background />
             </div>
         );
     }
---
+++ src/components/Header/parts/Background/Background.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import cn from 'classnames/bind';
+
+import headerGif from './assets/header.gif';
+import headerJpg from './assets/header.jpg';
+
+import * as styles from './Background.scss';
+
+const cx = cn.bind(styles);
+
+class Background extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            loading: true,
+        };
+
+        const gif = new Image();
+        gif.onload = () => {
+            this.setState({
+                loading: false,
+            });
+        };
+        gif.src = headerGif;
+    }
+
+    render() {
+        const { loading } = this.state;
+
+        const temporaryBackgroundStyles = {
+            backgroundImage: `url(${headerJpg})`,
+        };
+
+        const styles = {
+            backgroundImage: `url(${headerGif})`,
+        };
+
+        /** It's important to save order of div's */
+        return (
+            <>
+                <div className={cx('root')} style={styles} />
+                {loading && (
+                    <div
+                        className={cx('root')}
+                        style={temporaryBackgroundStyles}
+                    />
+                )}
+            </>
+        );
+    }
+}
+
+export default Background;
